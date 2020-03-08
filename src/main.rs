5aa658fc1efa22a8b2524fac90118216b13649f5
Nick Robinson <robinsonn1@live.com>
2020-03-08 12:29:14 -0700

    Initial endpoint testing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/world")]
+fn world() -> &'static str {
+    "made it to the second endpoint!"
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/hello", routes![world])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
