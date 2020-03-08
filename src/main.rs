6b6937880bbbfaf79744076a88d18375e2e17441
Nick Robinson <robinsonn1@live.com>
2020-03-08 13:04:12 -0700

    More endpoint tests.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::http::RawStr;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -12,10 +14,24 @@ fn world() -> &'static str {
     "made it to the second endpoint!"
 }
 
+#[get("/world/<name>")]
+fn world_hello(name: &RawStr) -> String {
+    format!("Hello, {}!", name.as_str())
+}
+
+#[get("/world/<name>/<age>/<cool>")]
+fn world_hello_age(name: String, age:u8, cool: bool) -> String {
+    if cool {
+        format!("Your name is {}, you are {} years old and you are cool", name, age)
+    }
+    else {
+        format!("Your age is {}, but you aren't very cool {}", age, name)
+    }
+}
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![index])
-        .mount("/hello", routes![world])
+        .mount("/hello", routes![world, world_hello, world_hello_age])
 }
 
 fn main() {
